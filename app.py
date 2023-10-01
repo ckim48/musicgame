67cd714486173c0817a7386d5b0c2211fe78c857
ckim48 <ckim48@u.rochester.edu>
2023-10-01 14:00:44 +0900

    ss

--- app.py
+++ app.py
@@ -11,13 +11,27 @@ def index():
     else:
         return render_template('index.html', isLogin = False)
 
+def validate_login(username, password):
+    conn = sqlite3.connect('static/assets/data/database.db')
+    cursor = conn.cursor()
+
+    # Check if the provided username and password match a record in the database
+    cursor.execute("SELECT * FROM Users WHERE username = ? AND password = ?", (username, password))
+    user = cursor.fetchone()
+
+    conn.close()
+    return user
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        if username == "test" and password == "1234":
+
+        # Validate the login credentials
+        user = validate_login(username, password)
+
+        if user:
             session['username'] = username
             return redirect(url_for('index'))
         else:
@@ -31,8 +45,31 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
 
+
+def insert_user_data(username, password, age, country, email):
+    conn = sqlite3.connect('static/assets/data/database.db')
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO Users (username, password, age, country, email) VALUES (?, ?, ?, ?, ?)",
+                   (username, password, age, country, email))
+
+    conn.commit()
+    conn.close()
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        age = request.form.get('age')
+        country = request.form.get('country')
+        email = request.form.get('email')
+
+        insert_user_data(username, password, age, country, email)
+
+        return redirect(url_for('login'))
+
     return render_template('register.html', title='Register')
 @app.route('/scoreboard', methods=['GET', 'POST'])
 def scoreboard():
@@ -45,10 +82,13 @@ def scoreboard():
 
     if current_score is None:
         return 'User not found', 404
-
+    if 'username' in session:
+        isLogin = True
+    else:
+        isLogin = False
     # Increment the current score by 1
     new_score = current_score[0] + 1
-    return render_template('scoreboard.html', title='Scoreboard',score = current_score[0])
+    return render_template('scoreboard.html', title='Scoreboard',score = current_score[0], isLogin = isLogin)
 
 @app.route('/update_score', methods=['POST'])
 def update_score():
